045a94a4500d08ef3ef0c2e49497a2a154294706
Ketan Agrawal <agrawalk@stanford.edu>
2021-06-18 02:49:12 -0400

    updated front page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,133 +1,150 @@
 import Link from 'next/link'
-import { SetStateAction, useState } from 'react'
+import Image from 'next/image'
 
-interface ExpandableTitleProps {
-  title: string
-  titleHref?: string
-  expanded: boolean
-  setExpanded: React.Dispatch<SetStateAction<boolean>>
+interface NavItemProps {
+  href: string
+  isActive?: boolean
 }
 
-interface ExpandableProps {
-  title: string
-  titleHref?: string
-  flipped?: boolean
-}
-
-
-const ExpandableTitle: React.FC<ExpandableTitleProps> = ({ title, titleHref, expanded, setExpanded }) => (
-  titleHref ? (
-    <Link href={titleHref}>
-      <a>
-        <p
-          className={"cursor-pointer font-bold text-center " +
-            (!expanded ? "hover:text-pink-600" : "text-pink-600")}
-          onClick={() => setExpanded(!expanded)}>
-          {title}
-        </p>
+const NavItem: React.FC<NavItemProps> = ({ href, isActive, children }) => (
+  <li>
+    <Link href={href}>
+      <a
+        className={`font-display block px-4 py-2${isActive ? 'bg-blue-200 text-blue-700' : ''}`}
+      >
+        {children}
       </a>
     </Link>
-  ) : (
-      <p
-        className={"cursor-pointer font-bold text-center " +
-          (!expanded ? "hover:text-pink-600" : "text-pink-600")}
-        onClick={() => setExpanded(!expanded)}>
-        {title}
-      </p>
-    )
+  </li>
 )
 
-
-const Expandable: React.FC<ExpandableProps> = ({ title, titleHref, flipped, children }) => {
-  const [expanded, setExpanded] = useState(false)
-  const styles = "flex flex-col items-center space-y-4"
-  const titleP = <ExpandableTitle
-    title={title}
-    titleHref={titleHref}
-    expanded={expanded}
-    setExpanded={setExpanded}
-  />;
-  return (
-    <div
-      style={{ minWidth: "30%", maxWidth: "30%" }}
-      className={styles}
-    >
-      {!flipped && titleP}
-      {expanded && children}
-      {flipped && titleP}
-    </div>
-  )
-}
-
-interface MyLinkProps {
-  href: string
-}
-const MyLink: React.FC<MyLinkProps> = ({ href, children }) => (
-  <a className="text-blue-600 hover:underline" href={href}>
-    {children}
-  </a>
+const Nav = () => (
+  <nav className="p-4 flex justify-between">
+    <ul className="flex space-x-2 items-center">
+      <NavItem href="/"><h1 className="font-bold text-2xl">Ketan Agrawal</h1></NavItem>
+    </ul>
+    <ul className="flex items-center">
+      <NavItem href="https://twitter.com/_ketan0"> <Image src="/twitter.svg" width="25" height="25" /> </NavItem>
+      <NavItem href="https://github.com/ketan0"> <Image src="/github.svg" width="25" height="25" /> </NavItem>
+      <NavItem href="https://www.linkedin.com/in/ketan-jay-agrawal/"> <Image src="/linkedin.svg" width="25" height="25" /> </NavItem>
+    </ul >
+  </nav >
 )
 
-const IndexPage = () => {
-  const [darkTheme, setDarkTheme] = useState(true)
-  return (
-    <div className={`h-screen flex flex-col justify-start ${darkTheme ? "text-white bg-black" : "text-black bg-white"
-      }`}>
-      <button className="self-end" onClick={() => setDarkTheme(!darkTheme)}>{darkTheme ? "light theme" : "dark theme"}</button>
-      <div className="flex flex-col space-y-8 justify-center items-stretch h-full">
-        <div style={{ minHeight: "20%", maxHeight: "20%" }} className="flex flex-row justify-around items-end">
-          <Expandable title="Who am I?" flipped>
-            <p>
-              Well, I'm still figuring that out.
-              But for now, I'm a Stanford student, studying Symbolic Systems and Computer Science.
-              I spend my free time&nbsp;
-                            <MyLink href="https://twitter.com/_ketan0/status/1333332491365658624">drawing</MyLink>,
-        writing notes in my digital garden,
-        and building whatever I can dream up.
-                        </p>
-          </Expandable>
-          <Expandable title="What makes me tick?" flipped>
-            <p>
-              I am most interested in studying fascinating, complex systems,
-              whether they be made of neurons or transistors. I am dreaming about new ways
-              that humans and computers can interact with each other.
-            </p>
-          </Expandable>
-          <Expandable title="Hit me up" flipped>
-            <p>
-              I'm always looking to make new friends on&nbsp;
-                <MyLink href="https://twitter.com/_ketan0">
-                Twitter
-                </MyLink>!
-                You can also send me an&nbsp;
-                <MyLink href="mailto:agrawalk@stanford.edu">email</MyLink>,
-                or check out my open-source projects on&nbsp;
-                <MyLink href="https://github.com/ketan0/">Github</MyLink>.
-            </p>
-          </Expandable>
+const IndexPage = () => (
+  <div className="divide-y divide-gray-200">
+    <Nav />
+    <div className="flex justify-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 p-8 gap-16 max-w-5xl">
+        <div className="flex justify-center items-center">
+          <Image className="rounded-full" src="/me.jpg" alt="Picture of Ketan Agrawal" width="400" height="400" quality="100" />
         </div>
-        <p className="text-7xl md:text-9xl self-center text-center">
-          <span style={{ "color": "rgba(50,141,242)", }}>Ketan </span>
-          <span style={{ "color": "rgba(46,0,213)", }}>Agrawal</span>
-          {/*<span style={{ "color": "rgba(152,0,218)", }}>Agrawal</span>*/}
-        </p>
-
-        <div style={{ minHeight: 250 }} className="flex flex-row justify-around items-start">
-          <Expandable title="Explore my writing">
-            <p>Coming soon!</p>
-          </Expandable>
-          <Expandable
-            title="Explore my digital garden 🌱"
-            titleHref="https://garden.ketan.me"
-          />
+        <div className="flex flex-col space-y-4">
+          <h1 className="font-bold text-xl">About Me</h1>
+          <p>
+            Hi! I'm a fourth-year student at Stanford University studying
+            Symbolic Systems (B.S.) and Computer Science (M.S.) During my time
+            at Stanford, I've been involved with several&nbsp;
+            <Link href="#research_interests">
+              <a className="normal-link"> research labs</a>
+            </Link>
+            , with a broad focus on human-centered artificial
+            intelligence. I also enjoy creating&nbsp;
+    <Link href="#projects">
+              <a className="normal-link">projects</a>
+            </Link> that entertain me. My
+    pet interests include customizing Emacs, digital gardening, and my
+    cats.
+          </p>
+          <p>
+            For more info, please see <Link href="/resume.pdf"><a className="normal-link">my resume</a></Link>.
+          </p>
+        </div>
+        <div className="flex flex-col space-y-4">
+          <a id="research_interests">
+            <h1 className="font-bold text-xl">Research Interests</h1>
+          </a>
+          <p>
+            A common thread through my research is that I'm interested in using
+            artificial intelligence to cooperate with and benefit humanity.
+          </p>
+          <p>
+            In my work with the&nbsp;
+            <Link href="https://langcog.stanford.edu"><a className="normal-link">Language and Cognition Lab</a></Link>
+            , I applied modern computer vision algorithms to a large, naturalistic
+            dataset of infant headcam footage. The analysis helped shed light on how
+            babies' perceptual scenes change over time, and was accepted as a talk at
+            CogSci 2020.
+          </p>
+          <p>
+            With the&nbsp;
+            <Link href="https://aicare.stanford.edu"><a className="normal-link"> Stanford Partnership in AI-Assisted Care</a></Link>
+            , I have been exploring how we can use multimodal neural networks
+            for automated screening of mental health problems, such as
+            depression and anxiety.
+          </p>
+          <p>
+            Most recently, I have been working in the&nbsp;
+              <Link href="https://hci.stanford.edu"><a className="normal-link">Stanford HCI Group</a></Link>
+            , building a novel system for human-AI music co-creation.
+          </p>
+        </div>
+        <div className="flex flex-col space-y-4">
+          <a id="projects"><h1 className="font-bold text-xl">Projects</h1></a>
+          <div className="grid grid-cols-2 w-80 h-80 md:w-96 md:h-96">
+            <Link href="https://github.com/ketan0/smart-glove">
+              <a>
+                <div className="relative w-5/6 h-5/6">
+                  <Image src="/glove.jpg" layout="fill" objectFit="contain" />
+                  <div className="absolute inset-0 flex justify-center items-center transition duration-500 ease-in-out bg-gray-800 bg-opacity-0 opacity-0 hover:bg-opacity-50 hover:opacity-100">
+                    <p className="font-sans font-bold text-white text-center">SMART GLOVE</p>
+                  </div>
+                </div>
+              </a>
+            </Link>
+            <Link href="https://github.com/ketan0/org-twitter">
+              <a>
+                <div className="relative w-5/6 h-5/6">
+                  <Image src="/org-twitter.png" layout="fill" objectFit="cover" />
+                  <div className="absolute inset-0 flex justify-center items-center transition duration-500 ease-in-out bg-gray-800 bg-opacity-0 opacity-0 hover:bg-opacity-50 hover:opacity-100">
+                    <p className="font-sans font-bold text-white text-center">ORG-TWITTER</p>
+                  </div>
+                </div>
+              </a>
+            </Link>
+            <Link href="https://garden.ketan.me/">
+              <a>
+                <div className="relative w-5/6 h-5/6">
+                  <Image src="/seedling-solid.svg" layout="fill" objectFit="cover" />
+                  <div className="absolute inset-0 flex justify-center items-center transition duration-500 ease-in-out bg-gray-800 bg-opacity-0 opacity-0 hover:bg-opacity-50 hover:opacity-100">
+                    <p className="font-sans font-bold text-white text-center">DIGITAL GARDEN</p>
+                  </div>
+                </div>
+              </a>
+            </Link>
+            <Link href="https://github.com/ketan0/selfdriving-rc">
+              <a>
+                <div className="relative w-5/6 h-5/6">
+                  <Image src="/selfdrivingcar.jpg" layout="fill" objectFit="contain" />
+                  <div className="absolute inset-0 flex justify-center items-center transition duration-500 ease-in-out bg-gray-800 bg-opacity-0 opacity-0 hover:bg-opacity-50 hover:opacity-100">
+                    <p className="font-sans font-bold text-white text-center">AUTONOMOUS RC CAR</p>
+                  </div>
+                </div>
+              </a>
+            </Link>
+          </div>
         </div>
       </div>
-      <p className="self-center"><MyLink href="/resume.pdf">Resume</MyLink></p>
-    </div >
-  )
-}
-
-// interesting red-purplish: 173,0,96
-// <span style={{ "color": "rgba(57,255,20)", }}>Agrawal</span>
+    </div>
+    <footer className="flex flex-col items-center text-center py-4 space-y-2">
+      <p>© 2021 Ketan Agrawal.</p>
+      <p className="text-sm">
+        This site was built with&nbsp;
+            <Link href="https://nextjs.org/"><a className="normal-link">Next.js</a></Link> and hosted on&nbsp;
+            <Link href="https://netlify.com/"><a className="normal-link">Netlify</a></Link>.
+            Source code is available <Link href="https://github.com/ketan0/ketan.me"><a className="normal-link">here</a></Link>.
+        </p>
+    </footer>
+  </div>)
 
 export default IndexPage

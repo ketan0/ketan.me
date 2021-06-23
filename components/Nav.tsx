84540e07cf461e145259831fa861f2c9c935f5a3
Ketan Agrawal <agrawalk@stanford.edu>
2021-06-23 00:51:26 -0400

    remove laboratory from navbar (for now)

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -17,11 +17,11 @@ const NavItem: React.FC<NavItemProps> = ({ href, isActive, children }) => (
   </li>
 )
 
+// <NavItem href="https://laboratory.ketan.me/"><h1>Laboratory</h1></NavItem>
 const Nav = () => (
   <nav className="p-4 flex justify-between">
     <ul className="flex space-x-2 items-center">
       <NavItem href="/"><h1 className="font-bold text-2xl">Ketan Agrawal</h1></NavItem>
-      <NavItem href="https://laboratory.ketan.me/"><h1>Laboratory</h1></NavItem>
     </ul>
     <ul className="flex items-center">
       <NavItem href="https://twitter.com/_ketan0">
